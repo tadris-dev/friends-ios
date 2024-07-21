819ade32b5e99a70cec8d895474295b281d245ea
Tim Fraedrich <timfraedrich@icloud.com>
2024-07-21 15:23:43 +0200

    Initial Commit

---
+++ Friends/FriendsApp.swift
@@ -0,0 +1,17 @@
+//
+//  FriendsApp.swift
+//  Friends
+//
+//  Created by Tim Fraedrich on 21.07.24.
+//
+
+import SwiftUI
+
+@main
+struct FriendsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
