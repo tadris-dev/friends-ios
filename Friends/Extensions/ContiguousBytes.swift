248111e2e4a70e685afb40fc8e3f7a1e8e9d6ac8
Tim Fraedrich <timfraedrich@icloud.com>
2024-11-17 01:53:17 +0100

    Add useful extensions

---
+++ Friends/Extensions/ContiguousBytes.swift
@@ -0,0 +1,13 @@
+import CryptoKit
+import Foundation
+
+extension ContiguousBytes {
+    
+    /// A Data instance created safely from the contiguous bytes without making any copies.
+    var dataRepresentation: Data {
+        return self.withUnsafeBytes { bytes in
+            let cfdata = CFDataCreateWithBytesNoCopy(nil, bytes.baseAddress?.assumingMemoryBound(to: UInt8.self), bytes.count, kCFAllocatorNull)
+            return ((cfdata as NSData?) as Data?) ?? Data()
+        }
+    }
+}
