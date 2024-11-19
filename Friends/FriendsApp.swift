4acd00ddca1af48878267bf6afdf498898dccf41
Tim Fraedrich <timfraedrich@icloud.com>
2024-11-19 17:44:50 +0100

    remove unused code

--- Friends/FriendsApp.swift
+++ Friends/FriendsApp.swift
@@ -29,15 +29,3 @@ struct FriendsApp: App {
         self.appState = FriendsAppState(friends: Friend.exampleFriends)
     }
 }
-
-@Model
-class LocationSample {
-    
-    var latitude: Double
-    var longitude: Double
-    
-    init(latitude: Double, longitude: Double) {
-        self.latitude = latitude
-        self.longitude = longitude
-    }
-}
