4d5a21ed041343be73291fb725fa0df0696b68bb
Tim Fraedrich <timfraedrich@icloud.com>
2025-01-13 11:41:34 +0100

    extend Publisher

--- Friends/Extensions/Publisher.swift
+++ Friends/Extensions/Publisher.swift
@@ -6,6 +6,10 @@ extension Publisher {
         mapError { $0 }
     }
     
+    func ignoreNil<T>() -> some Publisher<T, Failure> where Output == Optional<T> {
+        compactMap { $0 }
+    }
+    
     func mapToVoid() -> some Publisher<Void, Failure> {
         map { _ in () }
     }
