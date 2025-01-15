d7c72a4636429095eb8330220f3347c0a0f6bfad
Tim Fraedrich <timfraedrich@icloud.com>
2025-01-15 15:10:28 +0100

    modify CryptoStore

--- Friends/Models/Services/CryptoStore.swift
+++ Friends/Models/Services/CryptoStore.swift
@@ -4,69 +4,16 @@ import Security
 
 struct CryptoStore {
     
-    // MARK: - RSA Key Storage
-    
-    /// Stores an RSA key in the keychain.
-    func storeKey(_ key: SecKey, label: String) throws {
-        
-        let query = [
-            kSecClass: kSecClassKey,
-            kSecAttrApplicationLabel: label,
-            kSecAttrAccessible: kSecAttrAccessibleWhenUnlocked,
-            kSecUseDataProtectionKeychain: true,
-            kSecValueRef: key,
-        ] as [String: Any]
-        
-        let status = SecItemAdd(query as CFDictionary, nil)
-        guard status == errSecSuccess else {
-            throw CryptoStoreError("Unable to store item: \(status.message)")
-        }
-   }
-    
-    /// Reads an RSA key from the keychain.
-    func readKey(label: String) throws -> SecKey? {
-        let query = [
-            kSecClass: kSecClassKey,
-            kSecAttrApplicationLabel: label,
-            kSecAttrKeyType: kSecAttrKeyTypeRSA,
-            kSecUseDataProtectionKeychain: true,
-            kSecReturnRef: true,
-        ] as [String: Any]
-        
-        var item: CFTypeRef?
-        switch SecItemCopyMatching(query as CFDictionary, &item) {
-        case errSecSuccess: return (item as! SecKey)
-        case errSecItemNotFound: return nil
-        case let status: throw CryptoStoreError("Keychain read failed: \(status.message)")
-        }
-    }
-    
-    /// Removes any existing RSA key with the given label.
-    func deleteKey(label: String) throws {
-        let query = [
-            kSecClass: kSecClassKey,
-            kSecUseDataProtectionKeychain: true,
-            kSecAttrApplicationLabel: label,
-        ] as [String: Any]
-        switch SecItemDelete(query as CFDictionary) {
-        case errSecItemNotFound, errSecSuccess: break // Ignore these.
-        case let status:
-            throw CryptoStoreError("Unexpected deletion error: \(status.message)")
-        }
-    }
-    
-    // MARK: - Symmetric Key Storage
-    
-    /// Stores a SymmetricKey in the keychain as a generic password.
-    func storeKey(_ key: SymmetricKey, account: String) throws {
-        
+    /// Stores data in the keychain as a generic password.
+    func storeData(_ data: Data, account: String, service: String) throws {
         let query = [
             kSecClass: kSecClassGenericPassword,
-            kSecAttrAccount: account,
-            kSecAttrService: "Test" as CFString,
+            kSecAttrAccount: account as CFString,
+            kSecAttrService: service as CFString,
             kSecAttrAccessible: kSecAttrAccessibleWhenUnlocked,
             kSecUseDataProtectionKeychain: true,
-            kSecValueData: key.rawRepresentation as CFData,
+            kSecAttrSynchronizable: false,
+            kSecValueData: data as CFData,
         ] as [String: Any]
         
         let status = SecItemAdd(query as CFDictionary, nil)
@@ -75,34 +22,31 @@ struct CryptoStore {
         }
     }
     
-    /// Reads a SymmetricKey from the keychain as a generic password.
-    func readKey(account: String) throws -> SymmetricKey? {
-        
+    /// Reads data from the keychain as a generic password.
+    func readData(account: String, service: String) throws -> Data? {
         let query = [
             kSecClass: kSecClassGenericPassword,
-            kSecAttrAccount: account,
-            kSecAttrService: "Test" as CFString,
+            kSecAttrAccount: account as CFString,
+            kSecAttrService: service as CFString,
             kSecUseDataProtectionKeychain: true,
             kSecReturnData: true,
         ] as [String: Any]
         
         var item: CFTypeRef?
         switch SecItemCopyMatching(query as CFDictionary, &item) {
-        case errSecSuccess:
-            guard let data = item as? Data else { return nil }
-            return try SymmetricKey(rawRepresentation: data)
+        case errSecSuccess: return item as? Data
         case errSecItemNotFound: return nil
         case let status: throw CryptoStoreError("Keychain read failed: \(status.message)")
         }
     }
     
-    /// Removes any existing SymmetricKey with the given account.
-    func deleteKey(account: String) throws {
+    /// Removes existing data saved as a generic password.
+    func deleteData(account: String, service: String) throws {
         let query = [
             kSecClass: kSecClassGenericPassword,
+            kSecAttrAccount: account as CFString,
+            kSecAttrService: service as CFString,
             kSecUseDataProtectionKeychain: true,
-            kSecAttrAccount: account,
-            kSecAttrService: "Test" as CFString,
         ] as [String: Any]
         switch SecItemDelete(query as CFDictionary) {
         case errSecItemNotFound, errSecSuccess: break
@@ -123,12 +67,5 @@ struct CryptoStore {
         public var description: String {
             return message
         }
-    }
-    
-    private static func determineCFError(_ error: Unmanaged<CFError>?) -> Error {
-        guard let error = error?.takeRetainedValue() else {
-            return CryptoStoreError("Something went wrong while interfacing with a CryptoStore.")
-        }
-        return error
     }
 }
